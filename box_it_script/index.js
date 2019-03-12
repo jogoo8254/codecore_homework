a6690bc6421ce12b564e2c707af884109fb6684e
jogoo8254 <kev_inc88@hotmail.com>
2019-03-11 17:25:47 -0700

    finally got final stretch to work

--- box_it_script/index.js
+++ box_it_script/index.js
@@ -13,20 +13,20 @@ function drawLine(n){
     return line
 }
 // To do
-function drawTopBorder(n){
-    return `\u250C${drawLine(n)}\u2510`
+function drawTopBorder(n,beginning='\u250C',ending='\u2510'){
+    return `${beginning}${drawLine(n)}${ending}`
 }
 // To do
-function drawMiddleBorder(n){
-    return `|${drawLine(n)}|`
+function drawMiddleBorder(n,beginning,ending='|'){
+    return `${beginning}${drawLine(n)}${ending}`
 }
 // To do
-function drawBottomBorder(n){
-    return `\u2514${drawLine(n)}\u2518`
+function drawBottomBorder(n,beginning='\u2514',ending='\u2518'){
+    return `${beginning}${drawLine(n)}${ending}`
 }
 // To do
-function drawBarsAround(s){
-    return `|${s}|`
+function drawBarsAround(s,beginning='|',ending='|'){
+    return `${beginning}${s}${ending}`
 }
 // To do
 function boxIt(fileContents){
@@ -35,21 +35,19 @@ function boxIt(fileContents){
         firstSet.push(content.split(',')[0])
         secondSet.push(content.split(',')[1])
     }
- 
     let firstSet_maxLength = getMaxLength(firstSet)
     let secondSet_maxLength = getMaxLength(secondSet)
 
-    let output=drawTopBorder(firstSet_maxLength)+drawTopBorder(secondSet_maxLength)
+    let output=drawTopBorder(n=firstSet_maxLength,beginning='\u250C',ending='\u252c')+drawTopBorder(secondSet_maxLength,beginning='')
     for(let i=0; i<fileContents.length; i++){
         let firstSetSpaces = getRemainingSpaces(firstSet_maxLength,firstSet[i])
         let secondSetSpaces = getRemainingSpaces(secondSet_maxLength,secondSet[i])
-
-        output+='\n'+drawBarsAround(firstSet[i]+firstSetSpaces)+drawBarsAround(secondSet[i]+secondSetSpaces)
+        output+='\n'+drawBarsAround(firstSet[i]+firstSetSpaces)+drawBarsAround(n=(secondSet[i]+secondSetSpaces),beginning='')
         if(i !== fileContents.length-1){
-            output+='\n'+drawMiddleBorder(firstSet_maxLength)+drawMiddleBorder(secondSet_maxLength)
+           output+='\n'+drawMiddleBorder(firstSet_maxLength,beginning='|',ending='\u253c')+drawMiddleBorder(secondSet_maxLength,beginning='')
         }
     }
-    output+='\n'+drawBottomBorder(firstSet_maxLength)+drawBottomBorder(secondSet_maxLength)
+    output+='\n'+drawBottomBorder(firstSet_maxLength,beginning='\u2514',ending='\u2534')+drawBottomBorder(secondSet_maxLength,beginning='')
     return output
 }
 
@@ -78,5 +76,6 @@ fs.readFile(fileName,(err, data) =>{
     if(err) throw err;
     let fileContents = data.toString().split('\n')
     console.log(boxIt(fileContents))
+    // console.log(fileContents)
     // boxIt(fileContents)
 })
