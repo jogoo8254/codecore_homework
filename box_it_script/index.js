45aa66b7e61ba3b7e3da8791e61fd3dcd6b75569
jogoo8254 <kev_inc88@hotmail.com>
2019-03-14 18:13:40 -0700

    modify index.js to support both reading from file and reading and terminal; also add readme.md

--- box_it_script/index.js
+++ box_it_script/index.js
@@ -4,27 +4,24 @@
  * Author: Kevin Njunge
  * Assignment: Box-It-Script
  * Due Date: March 15, 2019
+ * 
+ * 
+ * To execute, either: 
+ * 1.) run ./index.js <filename> with either .csv or .txt extension to read from file. Or
+ * 2.) run ./index.js <string 1> <string 2> ... to read from terminal.
  * **/
 
 
 // #! /usr/bin/env node -> runs env command which finds where 
 // node.js is installed and runs it.
-// inorder to make file: boxit.js executable with ./ you'll first need
-// to run chmod +x boxit.js
+// inorder to make file: index.js executable with ./ you'll first need
+// to run chmod +x index.js
 // https://jrgraphix.net/r/Unicode/2500-257F
 
 
 // grab file name
 // https://www.geeksforgeeks.org/javascript-program-to-read-text-file/
-const fileName = process.argv[2]
-// fetch and read the contents of file
-const fs = require('fs')
-fs.readFile(fileName,(err, data) =>{
-    if(err);
-    let fileContents = data.toString().split('\n')
-    console.log(boxIt(fileContents))
-})
-
+const args = process.argv.slice(2)
 
 // schematic of box
 const box ={
@@ -58,7 +55,7 @@ function drawLine(n){
  * @param  {string} ending='\u2510'
  * drawTopBorder: draws top border of length n
  */
-function drawTopBorder(n,beginning=box.topLeftCorner,ending=box.topRightCorner){
+function drawTopBorder(n, beginning= box.topLeftCorner, ending=box.topRightCorner){
     return `${beginning}${drawLine(n)}${ending}`
 }
 /**
@@ -90,36 +87,6 @@ function drawBarsAround(s,beginning=box.verticalLine,ending=box.verticalLine){
 }
 
 /**
- * @param  {} fileContents
- * boxIt: by calling combination of other functions,
- * it draws a box with list of strings in the input. 
- */
-function boxIt(fileContents){
-    let firstSet=[],secondSet=[]
-    for(let content of fileContents){
-        if(content){
-            firstSet.push(content.split(',')[0])
-            secondSet.push(content.split(',')[1])
-        }
-    }
-    let firstSet_maxLength = getMaxLength(firstSet)
-    let secondSet_maxLength = getMaxLength(secondSet)
-    let output=drawTopBorder(n=firstSet_maxLength,beginning=box.topLeftCorner,ending=box.middleTopCorner)+drawTopBorder(secondSet_maxLength,beginning='')
-    if(firstSet.length || secondSet.length){
-        for(let i=0; i<fileContents.length; i++){
-            let firstSetSpaces = getRemainingSpaces(firstSet_maxLength,firstSet[i])
-            let secondSetSpaces = getRemainingSpaces(secondSet_maxLength,secondSet[i])
-            output+=box.newLine+drawBarsAround(firstSet[i]+firstSetSpaces)+drawBarsAround(n=(secondSet[i]+secondSetSpaces),beginning='')
-            if(i !== fileContents.length-1){
-               output+=box.newLine+drawMiddleBorder(firstSet_maxLength,beginning=box.verticalLine,ending=box.middleLine)+drawMiddleBorder(secondSet_maxLength,beginning='')
-            }
-        }    
-    }
-    output+=box.newLine+drawBottomBorder(firstSet_maxLength,beginning=box.bottomLeftCorner,ending=box.middleBottomCorner)+drawBottomBorder(secondSet_maxLength,beginning='')
-    return output
-}
-
-/**
  * @param  {int} maxLength
  * @param  {string} string
  * getRemainingSpaces: add spaces to input string so 
@@ -147,4 +114,66 @@ function getMaxLength(set){
         }
     }
     return maxLength
+}
+
+/**
+ * @param  {[string]} contents
+ * @param  {boolean} readFromFileFlag = true
+ * boxIt: by calling combination of other functions,
+ * it draws a box with list of strings in the input. 
+ */
+function boxIt(contents,readFromFileFlag=true){
+    let output = ''
+    if(readFromFileFlag){ // read from file
+        let firstSet=[],secondSet=[]
+        for(let content of contents){
+            if(content){
+                firstSet.push(content.split(',')[0])
+                secondSet.push(content.split(',')[1])
+            }
+        }
+        let firstSet_maxLength = getMaxLength(firstSet)
+        let secondSet_maxLength = getMaxLength(secondSet)
+        output=drawTopBorder(n=firstSet_maxLength,beginning=box.topLeftCorner,ending=box.middleTopCorner)+drawTopBorder(secondSet_maxLength,beginning='')
+        if(firstSet.length || secondSet.length){
+            for(let i=0; i<contents.length; i++){
+                let firstSetSpaces = getRemainingSpaces(firstSet_maxLength,firstSet[i])
+                let secondSetSpaces = getRemainingSpaces(secondSet_maxLength,secondSet[i])
+                output+=box.newLine+drawBarsAround(firstSet[i]+firstSetSpaces)+drawBarsAround(n=(secondSet[i]+secondSetSpaces),beginning='')
+                if(i !== contents.length-1){
+                   output+=box.newLine+drawMiddleBorder(firstSet_maxLength,beginning=box.verticalLine,ending=box.middleLine)+drawMiddleBorder(secondSet_maxLength,beginning='')
+                }
+            }    
+        }
+        output+=box.newLine+drawBottomBorder(firstSet_maxLength,beginning=box.bottomLeftCorner,ending=box.middleBottomCorner)+drawBottomBorder(secondSet_maxLength,beginning='')
+    }else{ // read from terminal
+        let maxLength = getMaxLength(contents)
+        output=drawTopBorder(maxLength)
+        for(let i=0; i<contents.length; i++){
+            let remainingSpaces=getRemainingSpaces(maxLength,contents[i])
+            output+=box.newLine+drawBarsAround(contents[i]+remainingSpaces)
+            if(i !== contents.length-1){
+                output+=box.newLine+drawMiddleBorder(maxLength)
+            }
+        }
+        output+=box.newLine+drawBottomBorder(maxLength)
+    }
+    return output
+}
+
+// if only one argument is provided in terminal with .csv or .txt extension,
+//  then grab contents from file with that name.
+// ie.) ./index.js character.csv will read contents from character.csv file. 
+if(args.length ==1 && (args[0].includes("\.csv") || args[0].includes("\.txt"))){
+    const fileName = args[0]
+    // fetch and read the contents of file
+    const fs = require('fs')
+    fs.readFile(fileName,(err, data) =>{
+        if(err);
+        let fileContents = data.toString().split('\n')
+        console.log(boxIt(fileContents))
+    })
+}else{// else read contents from terminal
+    // second parameter passed to boxIt is set to false to indicate its reading from terminal
+    console.log(boxIt(args,false))
 }
