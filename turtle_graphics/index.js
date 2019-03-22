8d19322a99e6a46583734e7fad8150e5b61b82cc
jogoo8254 <kev_inc88@hotmail.com>
2019-03-21 18:39:20 -0700

    update forward(), create updateTurtleTrack() and generateTurtleTracks

--- turtle_graphics/index.js
+++ turtle_graphics/index.js
@@ -4,7 +4,7 @@ const y_coord = arguments[3]
 const header = '0 1 2 3 4'
 const grid_material = '┼─'
 const grid = '0┼─┼─┼─┼─┼\n1┼─┼─┼─┼─┼\n2┼─┼─┼─┼─┼\n3┼─┼─┼─┼─┼\n4┼─┼─┼─┼─┼'
-const turtle_foot_print= '•' // – Starting Location
+// const turtle_foot_print= '•' // – Starting Location
 // const end_point = '*'  // – End Location
 const trail = '-' // trail
 //  0 1 2 3 4
@@ -18,53 +18,106 @@ class Turtle{
     constructor(x,y){
         this.x = x;
         this.y = y;
-        this.x_direction = true;
-        this.y_direction = false;
-        this.left = false;
-        this.right = false; // by default at starting point you move forward right
+        this.x_direction = 1; // move east by default
+        this.y_direction = 0;
+        this.turn_left = false;
+        this.turn_right = false; // by default at starting point you move forward right
+        this.turtle_tracks = [[this.x,this.y]]
     }
     // row to build start point
-    setStartPoint(){
-        initial_row=''
-        for(let i = 0; i < x; i++){
-            initial_row+=' '
+    // setStartPoint(){
+    //     let initial_row=''
+    //     for(let i = 0; i < x; i++){
+    //         initial_row+=' '
+    //     }
+    //     initial_row+=turtle_foot_print
+    //     return initial_row
+    // }
+    // update list of turtle tracks with latest coordinates
+    generateTurtleTracks(x_direction,continued_point,latest_point){
+        let update_x,update_y=0
+        for(let i = continued_point;i< latest_point; i++){
+            update_x = x_direction ? i : this.x
+            update_y = x_direction ? this.y : i
+            this.turtle_tracks.push([update_x,update_y])
         }
-        initial_row+=turtle_foot_print
-        return initial_row
     }
+    updateTurtleTrack(){
+        const get_prior_coordinates = this.turtle_tracks.push()
+        if(get_prior_coordinates[0]!==this.x){
+            if(get_prior_coordinates[0]<this.x){
+                generateTurtleTracks(true,(get_prior_coordinates[0]+1),(this.x+1))
+                // for(let i =;i< ; i++){
+                //     this.turtle_tracks.push([i,this.y])   
+                // }
+            }else{
+                generateTurtleTracks(true,this.x,get_prior_coordinates[0])
+                // for(let i =this.x; i <get_prior_coordinates[0]; i++){
+                //     this.turtle_tracks.push([i,this.y])   
+                // }
+            }
+        }else if(get_prior_coordinates[1]!==this.y){
+            if(get_prior_coordinates[1]<this.y){
+                generateTurtleTracks(false,(get_prior_coordinates[1] +1),(this.y+1))
+                // for(let i = get_prior_coordinates[1] +1; i<=this.y; i++){
+                //     this.turtle_tracks.push([this.x,i])   
+                // }
+            }else{
+                generateTurtleTracks(false,this.y,get_prior_coordinates[i])
+                // for(let i =this.y; i <get_prior_coordinates[1]; i++){
+                //     this.turtle_tracks.push([this.x,i])   
+                // }
+            }
+        }
+    }
+    
     // move forward # steps
     forward(steps){
-        if(this.left){
-        }else if(this.right){
-
+        if(!this.y_direction){
+            if(this.x_direction===1){
+            // east:
+            // x_direction=1, // moving in positive direction. -1 moves in negative direction
+            // y_direction=0  // -1 is default, indicating moving in x direction. else if 1, then switch to y direction.
+                this.x +=steps
+            }else if(this.x_direction===-1){
+            // west:
+            // x_direction=-1,
+            // y_direction =0
+                this.x -= steps
+            }
+        }else if(!this.x_direction){
+            if(this.y_direction===1){
+            // north:
+            // x_direction=0,
+            // y_direction = 1
+                this.y +=steps
+            }else if(this.y_direction===-1){
+            // south:
+            // x_direction = 0
+            // y_direction = -1
+                this.y_direction -=steps
+            }
         }
-        // else{
-        //     // default is moving forward right
-        //     // this.x_steps = steps
-        // }
-        let updated_trail = start_point
-        for(let i=0; i< steps*2-1;i++){
-            updated_trail+=trail
-        }
-        updated_trail+= end_point
-        this.x = this.x+steps
-        return updated_trail;
+        updateTurtleTrack()
     }
     // method to turn turtle left
     left(){
-        this.left = true;
-        this.right = false;  
+        this.turn_left = true;
+        this.turn_right= false;          
     }
     // method to turn turtle right
     right(){
-        this.right= true;
-        this.left = false;
+        this.turn_right= true;
+        this.turn_left = false;
     }
     allPoints(){
 
     }
     print(){
-        
+
+    }
+    move(steps){
+
     }
 }
 
@@ -91,3 +144,4 @@ function drawGrid(x=0,y=0){
 
 const turtle = new Turtle(0,0).forward(3)
 console.log(`${header}\n${drawGrid()}`)
+
