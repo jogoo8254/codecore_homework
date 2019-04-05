27676248d3629a7d504aae4d11dbd13b4f5a0763
jogoo8254 <kev_inc88@hotmail.com>
2019-04-05 02:34:39 -0700

    modify root.js

--- super_team_picker/routes/root.js
+++ super_team_picker/routes/root.js
@@ -71,7 +71,7 @@ router.get("/cohorts/new", (req, res) => {
   });
   // NAME: cluck#show, METHOD: GET, PATH: /clucks/:id
   //            ðŸ‘‡ a wildcard match
-  router.get("/cohorts/:id", (req, res) => {
+  router.post("/cohorts/:id", (req, res) => {
     const id = req.params.id;
     knex("cohort")
       .where("id", id)
@@ -84,5 +84,47 @@ router.get("/cohorts/new", (req, res) => {
         }
       });
   });
+
+  function teamCount(members, quantity) {
+    const list_of_members = members.split(", ")
+    const length_list_members = list_of_members.length //10
+    const loopCount = Math.ceil(length_list_members / quantity) //3
+    let list_of_teams = []
+    let team = []
+    for(let i= 0; i < quantity; i++){
+      for(let j= 0; j < loopCount;j++){
+        let random_member = list_of_members(() =>{
+          return 0.5 - Math.random()
+        })[0].shift()//9
+        team.push(random_member)
+        if(list_of_members.length ==0)
+          break;
+      }
+      list_of_teams.push(team)
+      team = []
+    }
+    return list_of_teams
+  }
+
+  function numberPerTeam(members, quantity) {
+    const list_of_members = members.split(", ")
+    const length_list_members = list_of_members.length //10
+    const loopCount = Math.ceil(length_list_members / quantity) //3
+    let list_of_teams = []
+    let team = []
+    for(let i= 0; i < loopCount; i++){
+      for(let j= 0; j < quantity;j++){
+        let random_member = list_of_members(() =>{
+          return 0.5 - Math.random()
+        })[0].shift()//9
+        team.push(random_member)
+        if(list_of_members.length ==0)
+          break;
+      }
+      list_of_teams.push(team)
+      team = []
+    }
+    return list_of_teams
+  }
 
 module.exports = router;
