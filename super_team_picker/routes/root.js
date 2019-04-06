cf227ae3ccd45015900d76d4d72ad5716a3bac08
jogoo8254 <kev_inc88@hotmail.com>
2019-04-06 14:10:45 -0700

    what i have so far

--- super_team_picker/routes/root.js
+++ super_team_picker/routes/root.js
@@ -3,41 +3,45 @@ const router = express.Router();
 const knex = require("../db/client");
 
 router.get('/',(req,res)=>{
-    res.send('<div>welcome</div>'); // render('welcome') home
+    res.send('<div><h1>Welcome to Super Team Picker!</h1></div>'); // render('welcome') home
 });
-router.get('/cohorts',(req,res)=>{
-    res.redirect('/cohorts/new')
+router.get('/cohorts/new',(req,res)=>{
+  res.render('cohorts/new');
 });
 
-router.post('/',(req,res)=>{
-    res.redirect('/');
-})
-
-router.post('/sign_in', (req,res)=>{
-    res.redirect("/");
-});
-router.post('/sign_out',(req,res)=>{
-    res.redirect("/");
-});
-  
-// NAME: cohort#new, METHOD: GET, PATH: /cohorts/new
-router.get("/cohorts/new", (req, res) => {
+router.get("/cohorts/:id", (req, res) => {
   const id = req.params.id;
-  const name = req.params.name;
+  console.log('==============', req.query);
   knex("cohort")
-    // .where("name",name)
-    .orderBy("created_at", "DESC")
-    .then(cohorts => {
-      if (cohorts) {
-        res.render("cohorts/new", { cohorts });
+    .where("id", id)
+    .first()
+    .then(cohort => {
+      if (cohort) {
+        res.render("cohorts/single_cohort", {cohort});
       } else {
-        res.send(`Cannot find cohort with id=${id}`);
+        res.send(`Cannot find cluck with id=${id}`);
       }
     });
-  });
-  
+});
+
+// router.get("/cohorts/:id",(req,res) =>{
+//   const fullName = request.query.fullName;
+//   const favouriteColour = request.query.favouriteColour;
+//   const message = request.query.message;
+
+// })
+
+// app.get("/thank_you", (request, response) => {
+
+//   response.render("thankYou", {
+//     fullName: fullName,
+//     favouriteColour: favouriteColour,
+//     favouriteDay: request.query.favouriteDay,
+//     message: message
+//   });
+// });
   // NAME: cohort#create, METHOD: POST, PATH: /cohorts
-  router.post("/cohorts", (req, res) => {
+  router.post("/cohorts/new", (req, res) => {
     const logo_url = req.body.logo_url;
     const name = req.body.name;
     const members = req.body.members;
@@ -51,80 +55,62 @@ router.get("/cohorts/new", (req, res) => {
       .then(data => {
         const cohort = data[0];
         // -- EXECUTE SQL
-        res.redirect(`/cohorts`);
+        // res.render('cohorts/single_cohort');
+        res.redirect(`/cohorts/${cohort.id}`)
       });
   });
   
-  // NAME: cohort#show, METHOD: GET, PATH: /cohorts
-  router.get("/cohorts", (req, res) => {
-    const id = req.params.id;
-    knex("cohort")
-      // .where("id", id)
-      .orderBy("created_at", "DESC")
-      .then(cohorts => {
-        if (cohorts) {
-          res.render("cohorts/list_cohorts", { cohorts });
-        } else {
-          res.send(`Cannot find cohort with id=${id}`);
-        }
-      });
-  });
-  // NAME: cluck#show, METHOD: GET, PATH: /clucks/:id
-  //            ðŸ‘‡ a wildcard match
-  router.post("/cohorts/:id", (req, res) => {
-    const id = req.params.id;
-    knex("cohort")
-      .where("id", id)
-      .orderBy("created_at", "DESC")
-      .then(cohort => {
-        if (cohort) {
-          res.render("cohorts/single_cohort", { cohort });
-        } else {
-          res.send(`Cannot find cluck with id=${id}`);
-        }
-      });
-  });
+router.get('/cohorts',(req,res)=>{
+    res.render('cohorts/list_cohorts');
+});
 
-  function teamCount(members, quantity) {
-    const list_of_members = members.split(", ")
-    const length_list_members = list_of_members.length //10
-    const loopCount = Math.ceil(length_list_members / quantity) //3
-    let list_of_teams = []
-    let team = []
-    for(let i= 0; i < quantity; i++){
-      for(let j= 0; j < loopCount;j++){
-        let random_member = list_of_members(() =>{
-          return 0.5 - Math.random()
-        })[0].shift()//9
-        team.push(random_member)
-        if(list_of_members.length ==0)
-          break;
-      }
-      list_of_teams.push(team)
-      team = []
-    }
-    return list_of_teams
-  }
+router.post('/',(req,res)=>{
+    res.redirect('/');
+})
 
-  function numberPerTeam(members, quantity) {
-    const list_of_members = members.split(", ")
-    const length_list_members = list_of_members.length //10
-    const loopCount = Math.ceil(length_list_members / quantity) //3
-    let list_of_teams = []
-    let team = []
-    for(let i= 0; i < loopCount; i++){
-      for(let j= 0; j < quantity;j++){
-        let random_member = list_of_members(() =>{
-          return 0.5 - Math.random()
-        })[0].shift()//9
-        team.push(random_member)
-        if(list_of_members.length ==0)
-          break;
-      }
-      list_of_teams.push(team)
-      team = []
+
+
+function teamCount(members, quantity) {
+  const list_of_members = members.split(", ")
+  const length_list_members = list_of_members.length //10
+  const loopCount = Math.ceil(length_list_members / quantity) //3
+  let list_of_teams = []
+  let team = []
+  for(let i= 0; i < quantity; i++){
+    for(let j= 0; j < loopCount;j++){
+      let random_member = list_of_members(() =>{
+        return 0.5 - Math.random()
+      })[0].shift()//9
+      team.push(random_member)
+      if(list_of_members.length ==0)
+        break;
     }
-    return list_of_teams
+    list_of_teams.push(team)
+    team = []
   }
+  return list_of_teams
+}
+
+function numberPerTeam(members, quantity) {
+  const list_of_members = members.split(", ")
+  const length_list_members = list_of_members.length //10
+  const loopCount = Math.ceil(length_list_members / quantity) //3
+  let list_of_teams = []
+  let team = []
+  for(let i= 0; i < loopCount; i++){
+    for(let j= 0; j < quantity;j++){
+      let random_member = list_of_members(() =>{
+        return 0.5 - Math.random()
+      })[0].shift()//9
+      team.push(random_member)
+      if(list_of_members.length ==0)
+        break;
+    }
+    list_of_teams.push(team)
+    team = []
+  }
+  return list_of_teams
+}
+
 
 module.exports = router;
