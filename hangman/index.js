6c8319a383d699f9ac30aed6fe8eaff1b466c219
jogoo8254 <kev_inc88@hotmail.com>
2019-05-01 00:51:27 -0700

    almost working. need to fix a couple more issues

--- hangman/index.js
+++ hangman/index.js
@@ -1,5 +1,6 @@
 // line => '\u2501'
 $(document).ready(() => {
+    let index_of_toggled_image=0
     // setting default
     for(let i=1; i<7; i++){
         $(`#hangman-${i}`).toggle()
@@ -12,7 +13,7 @@ $(document).ready(() => {
 
     const choose_random_word = sample_words[Math.floor(Math.random() * sample_words.length)]
     console.log(choose_random_word)
-    const number_letters = choose_random_word.split('').length
+    const number_letters = choose_random_word.length
     console.log(number_letters)
     const line = '\u2501'
     let lines=''
@@ -25,6 +26,38 @@ $(document).ready(() => {
         const b_id=$('.button')[i].id
         $(`#${b_id}`).click(function(){
             $(`#${b_id}`).css("background-color","salmon");
+            if(checkLetter(b_id)){
+                addLetterToCorrespondingLine(b_id)
+            }else{
+                toggleNextImage();
+            }
         });
+    }
+    function checkLetter(letter){
+        if(choose_random_word.includes(letter)){
+            return true;
+        }
+        return false;
+    }
+    function addLetterToCorrespondingLine(letter){
+        for(let i = 0; i< number_letters;i++){
+            if(choose_random_word[i]===letter){
+                let spaces=''
+                for(let j = 0; j<=i;j++){
+                    spaces+='  '
+                }
+                $('#input_slot').prepend($(`<div class="letter">${spaces}${letter.toUpperCase()}</div>`))                
+            }
+        }
+    }
+    function toggleNextImage(){
+        if(index_of_toggled_image!=6){
+            $(`#hangman-${index_of_toggled_image}`).toggle()
+            index_of_toggled_image +=1;
+            $(`#hangman-${index_of_toggled_image}`).toggle()
+            if(index_of_toggled_image==6){
+                $('.fullbody').append($(`<div class="game_over">GAME OVER</div>`))
+            }    
+        }
     }
 })
